ad6ec0853b0cb1f39a9845da26eafd9bbb543b37
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2023-11-09 09:15:42 -0800

    Include function load authorship contributions dataset

--- rs_graph/data/__init__.py
+++ rs_graph/data/__init__.py
@@ -1,7 +1,9 @@
 """Stored dataset loaders."""
 
+from dataclasses import dataclass
 from pathlib import Path
 
+from dataclasses_json import DataClassJsonMixin
 import numpy as np
 import pandas as pd
 
@@ -74,3 +76,111 @@ def load_rs_graph_embeddings_dataset() -> pd.DataFrame:
     embeddings["log_citation_count"] = np.log(embeddings.citation_count)
 
     return embeddings
+
+@dataclass
+class Contribution(DataClassJsonMixin):
+    repo: str
+    doi: str
+    author_position: int
+
+
+@dataclass
+class AuthorshipDetails(DataClassJsonMixin):
+    name: str
+    aliases: set[str]
+    h_index: int
+    contributions: list[Contribution]
+
+
+def load_rs_graph_author_contributions_dataset() -> pd.DataFrame:  # noqa: C901
+    # Load extended paper details dataset
+    paper_details_df = load_rs_graph_extended_paper_details_dataset()
+    repos_df = load_rs_graph_repos_dataset()
+
+    # Create a look up table for each author
+    all_author_contributions: dict[str, AuthorshipDetails] = {}
+    for _, author_row in paper_details_df.iterrows():
+        # Get DOI so we don't have to do a lot of getitems
+        doi = author_row["doi"]
+
+        # Get matching row in the repos dataset
+        repo_row = repos_df.loc[repos_df.doi == doi]
+
+        # Skip if no matching row
+        if len(repo_row) == 0:
+            continue
+        else:
+            repo_row = repo_row.iloc[0]
+
+        # Iter each author
+        for author_details in author_row["authors"]:
+            a_id = author_details["author_id"]
+
+            # Add new author
+            if a_id not in all_author_contributions:
+                # Get longest name in aliases to use as "cannonical name"
+                cannonical_name = author_details["name"]
+                if author_details["aliases"] is not None:
+                    for alias in author_details["aliases"]:
+                        if len(alias) > len(cannonical_name):
+                            cannonical_name = alias
+
+                # Convert aliases to set and "original author name"
+                aliases = (
+                    set(author_details["aliases"])
+                    if author_details["aliases"] is not None
+                    else set()
+                )
+                aliases.add(author_details["name"])
+
+                # Add new author
+                all_author_contributions[a_id] = AuthorshipDetails(
+                    name=cannonical_name,
+                    aliases=aliases,
+                    h_index=author_details["h_index"],
+                    contributions=[
+                        Contribution(
+                            repo=repo_row["repo"],
+                            doi=doi,
+                            author_position=author_details["author_position"],
+                        )
+                    ],
+                )
+
+            # Update existing author
+            else:
+                # Get existing author
+                existing_author_details = all_author_contributions[a_id]
+
+                # Always add new aliases
+                if author_details["aliases"] is not None:
+                    existing_author_details.aliases.update(author_details["aliases"])
+
+                # Always add possibly new name to aliases
+                existing_author_details.aliases.add(author_details["name"])
+
+                # Update cannonical name if need be
+                for alias in existing_author_details.aliases:
+                    if len(alias) > len(existing_author_details.name):
+                        existing_author_details.name = alias
+
+                # Add new contribution
+                existing_author_details.contributions.append(
+                    Contribution(
+                        repo=repo_row["repo"],
+                        doi=doi,
+                        author_position=author_details["author_position"],
+                    )
+                )
+
+    # Convert to dataframe
+    all_author_details_df = pd.DataFrame(
+        [
+            {
+                "author_id": author_id,
+                **author_details.to_dict(),
+            }
+            for author_id, author_details in all_author_contributions.items()
+        ]
+    )
+    return all_author_details_df
