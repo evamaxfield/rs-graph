e79ba1e50edbaf406680c3caaa66d5477262431b
evamaxfield <evamaxfieldbrown@gmail.com>
2024-03-27 11:55:03 -0700

    Do not resolve a filepath on a constant which may not exist yet

--- rs_graph/data/database/constants.py
+++ rs_graph/data/database/constants.py
@@ -2,5 +2,5 @@
 
 from .. import DATA_FILES_DIR
 
-PROD_DATABASE_FILEPATH = (DATA_FILES_DIR / "rs-graph-prod.db").resolve(strict=True)
-DEV_DATABASE_FILEPATH = (DATA_FILES_DIR / "rs-graph-dev.db").resolve(strict=True)
+PROD_DATABASE_FILEPATH = (DATA_FILES_DIR / "rs-graph-prod.db").resolve()
+DEV_DATABASE_FILEPATH = (DATA_FILES_DIR / "rs-graph-dev.db").resolve()
