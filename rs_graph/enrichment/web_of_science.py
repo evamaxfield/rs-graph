c9b989a5d4a26bffea843fcca8de90810493daf7
evamaxfield <evamaxfieldbrown@gmail.com>
2025-03-16 20:55:44 -0700

    Lint and format

--- rs_graph/enrichment/web_of_science.py
+++ rs_graph/enrichment/web_of_science.py
@@ -7,9 +7,9 @@ import time
 import traceback
 from dataclasses import dataclass
 
+import requests
 from dataclasses_json import DataClassJsonMixin
 from dotenv import load_dotenv
-import requests
 
 from .. import types
 
@@ -31,13 +31,19 @@ class NullGrantArticleSearchResult(DataClassJsonMixin):
     grant_id: str
     funder: str
 
+
 ###############################################################################
 
-def _get_articles_from_grant_id_and_funder(
+
+def _get_articles_from_grant_id_and_funder(  # noqa: C901
     grant_id: str,
     funder: str,
     wos_api_key: str,
-) -> list[types.DocumentWithGrantInformation] | NullGrantArticleSearchResult | types.ErrorResult:
+) -> (
+    list[types.DocumentWithGrantInformation]
+    | NullGrantArticleSearchResult
+    | types.ErrorResult
+):
     try:
         # Create a copy of params
         api_params = DEFAULT_API_PARAMS.copy()
@@ -50,7 +56,9 @@ def _get_articles_from_grant_id_and_funder(
         api_headers["X-ApiKey"] = wos_api_key
 
         # Request and handle response
-        response = requests.get(WEB_OF_SCIENCE_API_URL, params=api_params, headers=api_headers)
+        response = requests.get(
+            WEB_OF_SCIENCE_API_URL, params=api_params, headers=api_headers
+        )
         response.raise_for_status()
 
         # Parse response
@@ -69,7 +77,7 @@ def _get_articles_from_grant_id_and_funder(
                 error="Unexpected response structure",
                 traceback="",
             )
-        
+
         # Check if we have any records
         records = data["Data"]["Records"]["records"]
         if isinstance(records, str):
@@ -79,7 +87,7 @@ def _get_articles_from_grant_id_and_funder(
         # Check if we have any articles
         if "REC" not in records or len(records["REC"]) == 0:
             return []
-        
+
         # Iter over all records
         article_grant_pairs = []
         for record in records["REC"]:
@@ -88,20 +96,23 @@ def _get_articles_from_grant_id_and_funder(
                 "dynamic_data" not in record
                 or "cluster_related" not in record["dynamic_data"]
                 or "identifiers" not in record["dynamic_data"]["cluster_related"]
-                or "identifier" not in record["dynamic_data"]["cluster_related"]["identifiers"]
+                or "identifier"
+                not in record["dynamic_data"]["cluster_related"]["identifiers"]
             ):
                 continue
 
             # Get the DOI from the identifiers
-            article_identifiers = record["dynamic_data"]["cluster_related"]["identifiers"]["identifier"]
-            
+            article_identifiers = record["dynamic_data"]["cluster_related"][
+                "identifiers"
+            ]["identifier"]
+
             # Find the DOI
             paper_doi_or_none: str | None = None
             for identifier in article_identifiers:
                 if identifier["type"] == "doi":
                     paper_doi_or_none = identifier["value"]
                     break
-            
+
             # If we found a DOI, add to results
             if paper_doi_or_none is not None:
                 article_grant_pairs.append(
@@ -111,7 +122,7 @@ def _get_articles_from_grant_id_and_funder(
                         paper_doi=paper_doi_or_none,
                     )
                 )
-        
+
         if len(article_grant_pairs) == 0:
             # No articles found for this grant and funder
             return NullGrantArticleSearchResult(grant_id=grant_id, funder=funder)
@@ -126,19 +137,24 @@ def _get_articles_from_grant_id_and_funder(
             error=str(e),
             traceback=traceback.format_exc(),
         )
-    
+
+
 def get_articles_from_grant_id_and_funder(
     grant_id: str,
     funder: str,
     wos_api_key: str | None = None,
-) -> list[types.DocumentWithGrantInformation] | NullGrantArticleSearchResult | types.ErrorResult:
+) -> (
+    list[types.DocumentWithGrantInformation]
+    | NullGrantArticleSearchResult
+    | types.ErrorResult
+):
     # Get the API key from environment variables if not provided
     if wos_api_key is None:
         load_dotenv()
         wos_api_key = os.getenv("WOS_API_KEY")
         if wos_api_key is None:
             raise ValueError("WOS_API_KEY environment variable not set")
-        
+
     # Sleep for a bit to avoid hitting the API rate limit
     time.sleep(0.65)
 
