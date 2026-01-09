e5badb1b44b5a4fc9352299e2beea4214247c48f
evamaxfield <evamaxfieldbrown@gmail.com>
2026-01-09 14:24:49 -0800

    Remove JOSS break

--- rs_graph/sources/joss.py
+++ rs_graph/sources/joss.py
@@ -93,8 +93,6 @@ def get_dataset(
         # Increment page
         current_page += 1
 
-        break
-
         # Update progress
         if total_processed % 500 == 0:
             print(f"Processed {total_processed} papers")
